64bb4d2550600079944d2e5c9c61f4f4e9753f29
niuniu <1050266096@qq.com>
2016-05-10 20:34:44 +0800

    优化代码 判断目标网站是否更新,更新者生成txt文件or目标网站没有更新,就不更新txt文件

--- sock5/sock5.py
+++ sock5/sock5.py
@@ -14,7 +14,7 @@
 from urllib import  request
 import  time
 from bs4 import  BeautifulSoup
-import  os,sys
+import  os,sys,shutil
 import uuid
 import pymysql
 from multiprocessing.dummy import Pool as ThreadPool
@@ -34,7 +34,9 @@ def cur_file_dir():
 def get_html(url):
     req = request.Request(url)
     req.add_header('User-Agent',
-               'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36')
+               #'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36')
+    #req.add_header('User-Agent',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/3.5 Safari/536.11')
     f = request.urlopen(req)
     data = f.read()#.decode('gbk')
     f.close()
@@ -46,10 +48,54 @@ def get_html(url):
     allip=sockcontent.find_all('div')
     return allip
 
+#取得文本txt中的密码
+def is_text_update():
+    shutil.copy(cur_file_dir()+'\\'+"sock5代理服务器.txt",cur_file_dir()+'\\'+"sock5代理服务器copy.txt")
+    content=[]
+    try:
+
+        file_text=open(cur_file_dir()+'\\'+"sock5代理服务器copy.txt",'r')
+        content=file_text.readlines()
+        file_text.close()
+        one="".join(list(filter(str.isdigit, content[2])))
+        two="".join(filter(str.isdigit, content[8]))
+        three="".join(filter(str.isdigit, content[14]))
+        content=[one,two,three]
+        #return content
+        #return one
+        return content
+    except Exception as e:
+        print("出错了：",e)
+
+
+
+#取得目标网站代理密码组合成list
+def is_html_update(htmldata):
+    passlist=[]
+    for info in htmldata:
+    #服务器地址
+        allinfo=info.find_all('h4')
+        allinfo.pop() #删除末尾不需要的元素
+        #print(allinfo)
+
+
+        passlist.append("".join(list(filter(str.isdigit,allinfo[2].get_text()))))
+    return passlist
+
+
+        #取得A密码 纯数字形式
+
+
+
+
+
+
 def save_data_text(url):
     htmldata=get_html(url)
     f_text=""
-    #sockinfo=[]
+    #sockpassword=[]
+    html_update=is_html_update(htmldata)
+    text_update=is_text_update()
     for info in htmldata:
     #服务器地址
         allinfo=info.find_all('h4')
@@ -61,12 +107,42 @@ def save_data_text(url):
         hostpass=allinfo[3].get_text()
         hoststatus=allinfo[4].get_text()
 
+        #取得A密码 纯数字形式
+        #sockpassword.append("".join(list(filter(str.isdigit,hostpassword))))
+        #print(sockpassword)
+
+
+        #print(hostpassword)
+
+        if not(text_update[0]==html_update[0]) and not(text_update[1]==html_update[1] and not(text_update[2]==html_update[2])):
+
+                file_object = open(cur_file_dir()+'\\'+"sock5代理服务器.txt",'w+')
+                f_text+="{0}\n{1}\n{2}\n{3}\n{4}\n\n".format(hostip,hostport,hostpassword,hostpass,hoststatus)
+                file_object.writelines(f_text)
+                file_object.write('\n')
+                file_object.close()
+        else :
+                print("目标网站暂时没有更新")
+
+
+
+
+        """
         for i in range(1):
-            file_object = open(cur_file_dir()+'\\'+"sock5代理服务器.txt",'w+')
-            f_text+="{0}\n{1}\n{2}\n{3}\n{4}\n\n".format(hostip,hostport,hostpassword,hostpass,hoststatus)
-            file_object.writelines(f_text)
-            file_object.write('\n')
-            file_object.close()
+
+            print(sockpassword)
+            print(is_text_update()[1])
+
+            if not(is_text_update()[0]==sockpassword[0]) and not(is_text_update()[1]==sockpassword[1] and not(is_text_update()[2]==sockpassword[2])):
+
+                file_object = open(cur_file_dir()+'\\'+"sock5代理服务器t.txt",'w+')
+                f_text+="{0}\n{1}\n{2}\n{3}\n{4}\n\n".format(hostip,hostport,hostpassword,hostpass,hoststatus)
+                file_object.writelines(f_text)
+                file_object.write('\n')
+                file_object.close()
+            else :
+                print("目标网站暂时没有更新")
+           """
 
 
 
@@ -74,62 +150,20 @@ def save_data_text(url):
 
 
 if __name__=='__main__':
-   url=' '
-   time.sleep(5)
+   url=''
+
    print("采集开始...........")
+
    save_data_text(url)
+   
+   #print(is_text_update()[2])
+   #print(is_html_update(url))
    print("采集成功..............")
+   time.sleep(5)
+   os.remove(cur_file_dir()+'\\'+"sock5代理服务器copy.txt")
 
-'''''''''''''''''''''''''''''''''
-url='http://www.ishadowsocks.net/'
-req = request.Request(url)
-req.add_header('User-Agent',
-               'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36')
-f = request.urlopen(req)
-data = f.read()#.decode('gbk')
-f.close()
-soup = BeautifulSoup(data, 'lxml')
-html=soup.find(id="free").find(class_="container")
-
-sockcontent=html.find(class_='row').find_next(class_='row')
-
-allip=sockcontent.find_all('div')
-
-f_text=""
-sockinfo=[]
-for info in allip:
-    #服务器地址
-    allinfo=info.find_all('h4')
-    allinfo.pop() #要删除末尾的元素
-
-    print(allinfo)
-    hostip=allinfo[0].get_text()
-    hostport=allinfo[1].get_text()
-    hostpassword=allinfo[2].get_text()
-    hostpass=allinfo[3].get_text()
-    hoststatus=allinfo[4].get_text()
-
-    for i in range(1):
-         file_object = open(cur_file_dir()+'\\'+"sock5代理服务器.txt",'w+')
-         f_text+="{0}\n{1}\n{2}\n{3}\n{4}\n\n".format(hostip,hostport,hostpassword,hostpass,hoststatus)
-         file_object.writelines(f_text)
-         file_object.write('\n')
-         file_object.close()
-         #print(i.get_text())
-
-    #for  i in allinfo:
-
-          #file_object = open(cur_file_dir()+'\\'+"haha.txt",'w+')
-          #f_text=i.get_text()
-         # file_object.writelines(f_text)
-          #file_object.write('\n')
-          #file_object.close()
-          #print(i.get_text())
-
-
-    print('<br/>')
-
-'''''''''''''''''''''''''''''''''
+ 
+ 
 
 
 
